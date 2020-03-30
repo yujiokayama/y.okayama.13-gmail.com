bb63196f3e86e46865b1ac6462f74c27730645a5
yuji-okayama <y-okayama@tribeck.jp>
2020-03-30 10:32:18 +0900

    1st commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#使い方
+node server.js
+json-server db.json
---
+++ server.js
@@ -0,0 +1,30 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(
+  jsonServer.rewriter({
+    "/api/datareportlist": "/datareportlist"
+  })
+);
+
+// ミドルウェアの設定 (コンソール出力するロガーやキャッシュの設定など)
+server.use(middlewares);
+
+server.use(function(req, res, next) {
+  if (req.method === "POST") {
+    // POST送信を受ける場合、受けたPOSTレスポンスをGETに変更する
+    req.method = "GET";
+    // req.query = req.body;
+  }
+  // Continue to JSON Server router
+  next();
+});
+
+// db.json を基にデフォルトのルーティングを設定する
+server.use(router);
+// サーバをポート 3000 で起動する
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
